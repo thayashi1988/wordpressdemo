20fbef496628acdf2db43feb574b973294e8683d
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-28 23:47:45 +0900

    🐛 ハンバーガーメニューの固定css追加

--- corporate/src/assets/js/modules/Menu.ts
+++ corporate/src/assets/js/modules/Menu.ts
@@ -1,4 +1,5 @@
 export const Menu = () => {
+  const bodyElem = document.body;
   const hamburgerBtn = <HTMLElement>document.querySelector('.js-menu');
   const logoElem = <HTMLElement>document.querySelector('.m-header-logo');
   const layer = document.createElement('div');
@@ -6,18 +7,23 @@ export const Menu = () => {
 
   hamburgerBtn.addEventListener('click', (e: MouseEvent) => {
     e.preventDefault();
+    const scrollbarWidth = window.innerWidth - document.body.clientWidth;
     const hasLayer = document.querySelector('.l-menu-layer');
     if (hasLayer) {
       hamburgerBtn.classList.remove('is-open');
       logoElem.classList.remove('is-hide-logo');
       layer.classList.remove('is-layer-show');
+      bodyElem.classList.remove('is-fixed');
+      bodyElem.style.paddingRight = '';
       setTimeout(() => {
         hasLayer.remove();
       }, 400);
     } else {
       hamburgerBtn.classList.add('is-open');
       logoElem.classList.add('is-hide-logo');
-      document.body.appendChild(layer);
+      bodyElem.classList.add('is-fixed');
+      bodyElem.style.paddingRight = scrollbarWidth ? `${scrollbarWidth}px` : '';
+      bodyElem.appendChild(layer);
       setTimeout(() => {
         layer.classList.add('is-layer-show');
       }, 10);
