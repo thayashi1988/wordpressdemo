f24cf6350c4c2b8ef620478e92a152e26aa8f65c
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-03-01 00:07:14 +0900

    🐛 iOS時のモーダル背景固定処理追加

--- corporate/src/assets/js/modules/Menu.ts
+++ corporate/src/assets/js/modules/Menu.ts
@@ -3,12 +3,41 @@ export const Menu = () => {
   const hamburgerBtn = <HTMLElement>document.querySelector('.js-menu');
   const logoElem = <HTMLElement>document.querySelector('.m-header-logo');
   const layer = document.createElement('div');
+  const UA = window.navigator.userAgent.toLowerCase();
+  const isIOS = UA.includes('iphone') || UA.includes('ipod') || UA.includes('ipad');
+  console.log('isIOS:', isIOS);
   layer.classList.add('l-menu-layer');
 
   hamburgerBtn.addEventListener('click', (e: MouseEvent) => {
     e.preventDefault();
     const scrollbarWidth = window.innerWidth - document.body.clientWidth;
     const hasLayer = document.querySelector('.l-menu-layer');
+    if (isIOS) {
+      const scrollPosition = window.pageYOffset;
+      console.log('scrollPosition:', scrollPosition);
+      if (hasLayer) {
+        hamburgerBtn.classList.remove('is-open');
+        logoElem.classList.remove('is-hide-logo');
+        layer.classList.remove('is-layer-show');
+        bodyElem.style.position = '';
+        const backToScroll = parseInt(bodyElem.style.top) * -1;
+        bodyElem.style.top = '';
+        window.scrollTo(0, backToScroll);
+        setTimeout(() => {
+          hasLayer.remove();
+        }, 400);
+      } else {
+        hamburgerBtn.classList.add('is-open');
+        logoElem.classList.add('is-hide-logo');
+        bodyElem.style.position = 'fixed';
+        bodyElem.style.top = `-${scrollPosition}px`;
+        bodyElem.appendChild(layer);
+        setTimeout(() => {
+          layer.classList.add('is-layer-show');
+        }, 10);
+      }
+      return;
+    }
     if (hasLayer) {
       hamburgerBtn.classList.remove('is-open');
       logoElem.classList.remove('is-hide-logo');
